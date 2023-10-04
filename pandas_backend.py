4c69da6dc05b4af5e95dc89a8338361e6f4a9877
NgocNhuNguyenThi <nhu02349@gmail.com>
2023-10-05 01:29:27 +1000

    Feature 4

--- pandas_backend.py
+++ pandas_backend.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # You can set some options to better print pandas df
 # pd.set_option('display.max_columns', None)
 # pd.set_option('display.max_colwidth', 20)
 
+# Variables for feature 2
 offence_code = []
 offence_code_total = []
 
@@ -79,10 +81,51 @@ offence_code = df['OFFENCE_CODE'].unique().tolist()
 for i in offence_code:
     offence_code_total.append(len(df[df['OFFENCE_CODE']==i]))
 # Draw the bar graph
-plt.bar(offence_code, offence_code_total)
+fig = plt.figure(figsize=(10, 7))  # figure size
+
+plt.bar(np.arange(0, len(offence_code), step = 1), offence_code_total)
 plt.margins(x=0, y=0)
-# plt.xticks(np.arange(0, len(offence_code), step = 1), labels = offence_code)
-plt.title("Penalty case distribution") 
-plt.ylabel("Distribution") 
-plt.xlabel("Offence code") 
+plt.title("Penalty case distribution")
+plt.ylabel("Distribution")
+plt.xlabel("Offence code")
+plt.xticks(np.arange(0, len(offence_code), step = 1), labels=offence_code)
 plt.show()
+
+'''
+   ------------ Feature 4 and 5 --------------------
+   Analysing the cases caused by mobile phone usage, seatbelt, ... - ie: trend over time, offense code, and so on.
+'''
+
+'''' -------------- MOBILE PHONE USAGE ---------------------'''
+mobile_phone_df = df[df['MOBILE_PHONE_IND'] == 'Y']
+mobile_offence_year = [] 
+mobile_offence_total = []
+count = {}
+
+# pie chart show the percentage of mobile phone offence out of the total offences
+plt.pie([len(mobile_phone_df), len(df)], labels=['Mobile phone usage','Total'], autopct='%1.1f%%', explode=(0, 0.5))
+plt.show()
+
+# ---- Information ----
+info = mobile_phone_df.drop_duplicates(subset = 'OFFENCE_CODE')
+print(info.loc[:,['OFFENCE_CODE', 'OFFENCE_DESC', 'OFFENCE_FINYEAR', 'OFFENCE_MONTH', 'FACE_VALUE']])
+
+
+# ---- Trend ----
+# Use dictionary to count the total of mobile usage offence in each year
+for index, row in mobile_phone_df.iterrows():
+    year = row['OFFENCE_MONTH'][-4:] 
+    count[year] = count.get(year, 0) + 1
+
+# Separate the keys and values to correspoding list to draw the trend
+for key in sorted(count.keys()):
+    mobile_offence_year.append(key)
+    mobile_offence_total.append(count[key])
+
+plt.plot(mobile_offence_year, mobile_offence_total, 'r-') 
+plt.title("Trend of mobile phone usage offence") 
+plt.ylabel("Total of offence") 
+plt.xlabel("Year") 
+plt.ylim(30, 60)
+plt.show()
+
