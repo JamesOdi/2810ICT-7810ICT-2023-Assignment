02e3069ef621cf811fb1fc7cde74184e3389c1ca
NgocNhuNguyenThi <nhu02349@gmail.com>
2023-10-06 16:03:10 +1000

    Update all features

--- pandas/feature2.py
+++ pandas/feature2.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
-offence_code = []
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', 20)
+
+offence_year = []
 offence_code_total = []
+offence_count = {}
 
 df = pd.read_csv(r".\penalty_data_set_2.csv", low_memory=False)
 
@@ -11,16 +14,27 @@ df = pd.read_csv(r".\penalty_data_set_2.csv", low_memory=False)
    ------------ Feature 2 --------------------
    Produce a chart to show the distribution of cases in each offense code
 '''
-offence_code = df['OFFENCE_CODE'].unique().tolist()
-for i in offence_code:
-    offence_code_total.append(len(df[df['OFFENCE_CODE'] == i]))
-# Draw the bar graph
+offence_code = int(input("Enter offence code: ")) # take value from html tag
+
+data = df[df['OFFENCE_CODE'] == offence_code]
+
+# Use dictionary to count the total of mobile usage offence in each year
+
+for index, row in data.iterrows():
+   year = row['OFFENCE_FINYEAR']
+   offence_count[year] = offence_count.get(year, 0) + row['TOTAL_NUMBER']
+
+for key in sorted(offence_count.keys()):
+   offence_year.append(key)
+   offence_code_total.append(offence_count[key])
+
+# # Draw the bar graph
 fig = plt.figure(figsize=(10, 7))  # figure size
 
-plt.bar(np.arange(0, len(offence_code), step=1), offence_code_total)
-plt.margins(x=0, y=0)
+plt.bar(offence_year, offence_code_total)
 plt.title("Penalty case distribution")
-plt.ylabel("Distribution")
-plt.xlabel("Offence code")
-plt.xticks(np.arange(0, len(offence_code), step=1), labels=offence_code)
+plt.ylabel("Total of penalty notices issued")
+plt.xlabel("Year")
+plt.xticks(rotation=45)
+plt.savefig("offence_code_distribution.png")
 plt.show()
