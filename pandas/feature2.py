ca4cd7a8999452437a408163fe8d7644e8ea1e29
NgocNhuNguyenThi <nhu02349@gmail.com>
2023-10-08 12:48:19 +1000

    Update new csv file and user manual docx

--- pandas/feature2.py
+++ pandas/feature2.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime
 
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_colwidth', 20)
+# pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_colwidth', 20)
 
 offence_year = []
 offence_code_total = []
@@ -14,13 +15,30 @@ df = pd.read_csv(r".\penalty_data_set_2.csv", low_memory=False)
    ------------ Feature 2 --------------------
    Produce a chart to show the distribution of cases in each offense code
 '''
+
+from_date = input("From (dd/mm/yyyy) : ") # take value from html tag
+to_date = input("to (dd/mm/yyyy): ") # take value from html tag
+
+'''
+ IF FROM AND TO INPUT IS EMPTY -> USE DF DATAFRAME
+ IF FROM AND TO INPUT IS ENTERED -> USE FILTERED DATAFRAME
+'''
+
+# Convert the date to datetime64
+# df['OFFENCE_MONTH'] = pd.to_datetime(df['OFFENCE_MONTH'], format='%d/%m/%y')
+
+if from_date == '' and to_date == '':
+   data = df
+else:
+   data = df.loc[(df['OFFENCE_MONTH'] >= from_date) & (df['OFFENCE_MONTH'] <= to_date)]
+
 offence_code = int(input("Enter offence code: ")) # take value from html tag
 
-data = df[df['OFFENCE_CODE'] == offence_code]
+filtered_data = data[data['OFFENCE_CODE'] == offence_code]
 
 # Use dictionary to count the total of mobile usage offence in each year
 
-for index, row in data.iterrows():
+for index, row in filtered_data.iterrows():
    year = row['OFFENCE_FINYEAR']
    offence_count[year] = offence_count.get(year, 0) + row['TOTAL_NUMBER']
 
